74f5f37871c1a87000c818289f90ad1581649ac9
Harry <qdmfeng@163.com>
2020-02-07 20:27:29 -0500

    init base

--- Project03.py
+++ Project03.py
@@ -2,9 +2,40 @@ import datetime
 from prettytable import PrettyTable
 
 class Individual:
+    def __init__(self, id):
+        """a new instance for a individual"""
+        self.id = id
+        self.name = ''
+        self.gender = ''
+        self.bday = ''
+        self.age = ''
+        self.alive = True
+        self.dday = 'N/A'
+        self.child = 'N/A'
+        self.spouse = 'N/A'
 
-class family:
+class Family:
+    def __init__(self, id, married, divorced, hus, wife, child):
+        self.id = id
+        self.mar_date = married
+        self.div_date = divorced
+        self.hus_id = hus
+        self.wife_id = wife
+        self.child_id = child
+
 
 class Repository:
+    def __init__(self,d):
+        self.dir=d
+        self.indi={}
+        self.fam={}
+        self._analyze_files()
+
+    def _analyze_files(self):
+        pass
+
+    def pretty_print(self):
+        pass
+
 
 #This is Test branch
