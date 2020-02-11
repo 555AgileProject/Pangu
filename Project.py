a4d66c2822a2df38dc811bb16fd6cb7c93a585c2
Harry <qdmfeng@163.com>
2020-02-11 15:06:44 -0500

    Sprint 1 init

--- Project.py
+++ Project.py
@@ -40,9 +40,9 @@ class Repository:
     def update_age_alive(self):
         for indi_id, indi in self.indi.items():
             if indi.bday != 'NA':
-                birth_date = datetime.strptime(indi.bday,'%d %b %Y')
+                birth_date = datetime.strptime(indi.bday, '%d %b %Y')
                 age = datetime.now() - birth_date
-                indi.age = int(age.days/365)
+                indi.age = int(age.days / 365)
             else:
                 indi.age = 'NA'
             if indi.dday != 'NA':
@@ -53,31 +53,32 @@ class Repository:
     def _analyze_files(self):
         array = []
         tag = 'read_one_line'
-        indi_index = {'NAME':'name','SEX':'gender','BIRT':'bday','DEAT':'dday','FAMC':'child','FAMS':'spouse'}
-        fam_index = {'MARR':'mar_date','HUSB':'hus_id','WIFE':'wife_id','CHIL':'child_id','DIV':'div_date'}
+        indi_index = {'NAME': 'name', 'SEX': 'gender', 'BIRT': 'bday', 'DEAT': 'dday', 'FAMC': 'child',
+                      'FAMS': 'spouse'}
+        fam_index = {'MARR': 'mar_date', 'HUSB': 'hus_id', 'WIFE': 'wife_id', 'CHIL': 'child_id', 'DIV': 'div_date'}
         indi_buff = Individual(None)
-        indi_date_buff = [False,False]                        # 0 indicates bday 1 indicates dday 
-        fam_buff = Family(None,'NA','NA','NA','NA',set())
-        fam_date_buff = [False,False]                         # 0 indicates div_date 1 indicates mar_date
+        indi_date_buff = [False, False]  # 0 indicates bday 1 indicates dday
+        fam_buff = Family(None, 'NA', 'NA', 'NA', 'NA', set())
+        fam_date_buff = [False, False]  # 0 indicates div_date 1 indicates mar_date
         for eachrow in file_reading_gen(self.dir):
             readline = eachrow.split("|")
             if (readline[0] == '0'):
                 if (indi_buff.id != None):
-                     # condition 1 push the individual buffer
+                    # condition 1 push the individual buffer
                     new_indi = indi_buff
                     if new_indi.id not in self.indi.keys():
                         self.indi[new_indi.id] = new_indi
-                    indi_buff = Individual(None) #clear the buffer
-                    indi_date_buff = [False,False]                        # 0 indicates bday 1 indicates dday 
-                elif(fam_buff.id != None):
+                    indi_buff = Individual(None)  # clear the buffer
+                    indi_date_buff = [False, False]  # 0 indicates bday 1 indicates dday
+                elif (fam_buff.id != None):
                     # condition 2 push the family buffer
                     if (fam_buff.id != None):
                         new_fam = fam_buff
                     if new_fam.id not in self.fam.keys():
                         self.fam[new_fam.id] = new_fam
-                    fam_buff = Family(None,'NA','NA','NA','NA',set()) # clear the buffer
-                    fam_date_buff = [False,False]                         # 0 indicates div_date 1 indicates mar_date
-                if(indi_buff.id == fam_buff.id == None):
+                    fam_buff = Family(None, 'NA', 'NA', 'NA', 'NA', set())  # clear the buffer
+                    fam_date_buff = [False, False]  # 0 indicates div_date 1 indicates mar_date
+                if (indi_buff.id == fam_buff.id == None):
                     # condition 3 set the ?_buffer's id  
                     if (readline[1] in ["INDI", "FAM"]):
                         # Create New Family and push existing into the respective function
@@ -96,17 +97,17 @@ class Repository:
             elif (readline[0] == '1'):
                 if (indi_buff.id != None):
                     # condition 1 for update indi_buff
-                    if (readline[1] in ['BIRT','DEAT']):
+                    if (readline[1] in ['BIRT', 'DEAT']):
                         if readline[1] == 'BIRT':
                             indi_date_buff[0] = True
                         else:
                             indi_date_buff[1] = True
                     else:
-                        setattr(indi_buff,indi_index[readline[1]],readline[2])
+                        setattr(indi_buff, indi_index[readline[1]], readline[2])
 
-                elif(fam_buff.id != None):
+                elif (fam_buff.id != None):
                     # condition 2 for update fam_buff
-                    if (readline[1] in ['DIV','MARR']):
+                    if (readline[1] in ['DIV', 'MARR']):
                         if readline[1] == 'DIV':
                             fam_date_buff[0] = True
                         else:
@@ -114,7 +115,7 @@ class Repository:
                     elif readline[1] == 'CHIL':
                         fam_buff.child_id.add(readline[2])
                     else:
-                        setattr(fam_buff,fam_index[readline[1]],readline[2])
+                        setattr(fam_buff, fam_index[readline[1]], readline[2])
 
             elif (readline[0] == '2'):
                 if (readline[1] == 'DATE'):
@@ -134,8 +135,6 @@ class Repository:
                         fam_date_buff[1] = False
         self.update_age_alive()
 
-
-            
     def pretty_print(self):
         """ print out the pretty table of individual summary and family summary"""
         pti = PrettyTable(
@@ -152,4 +151,11 @@ class Repository:
                  d.child_id])
         print(ptf.get_string(title="Families"))
 
+    def us06(self):
+        '''Divorce can only occur before death of both spouses'''
+        pass
 
+    def us07(self):
+        '''Death should be less than 150 years after birth for dead people,
+        and current date should be less than 150 years after birth for all living people'''
+        pass
