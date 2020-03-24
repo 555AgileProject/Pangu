59be6cb7bfe8c9117f54e04764829c04a0427fba
Arun <arun.nalluri@gmail.com>
2020-03-24 16:24:37 -0400

    Updated test cases and fixed rolled back updates

--- test.py
+++ test.py
@@ -5,13 +5,25 @@ import unittest
 class UserStoryTest(unittest.TestCase):
     test = p.Repository('./Trump_Fam.ged')
 
+    def test_us01(self):
+        expect = {'@I32@', '@F4@', '@I6@', '@I31@', '@I7@', '@I14@', '@I34@'}
+        self.assertEqual(self.test.us01(), expect)
+
     def test_us02(self):
         expect = ["@F1@"]
         self.assertEqual(expect, self.test.us02())
 
     def test_us03(self):
         expect = ["@F1@"]
-        self.assertEqual(expect, self.test.us02())
+        self.assertEqual(expect, self.test.us03())
+
+    def test_us04(self):
+        expect = ['@F4@']
+        self.assertEqual(expect, self.test.us04())
+
+    def test_us05(self):
+        expect = {'@F4@'}
+        self.assertEqual(self.test.us05(), expect)
 
     def test_us06(self):
         expect = ['@F4@']
@@ -21,6 +33,10 @@ class UserStoryTest(unittest.TestCase):
         expect = ['@I12@', '@I13@']
         self.assertEqual(expect, self.test.us07())
 
+    def test_us08(self):
+        expect = ['@F4@', '@F4@']
+        self.assertEqual(expect, self.test.us08())
+
     def test_us09(self):
         expect = ['@F9@']
         self.assertEqual(expect, self.test.us09())
@@ -29,34 +45,26 @@ class UserStoryTest(unittest.TestCase):
         expect = ['@F1@', '@F9@']
         self.assertEqual(expect, self.test.us10())
 
-    def test_us04(self):
-        expect = ['@F4@']
-        self.assertEqual(expect, self.test.us04())
-
-    def test_us08(self):
-        expect = ['@F4@', '@F4@']
-        self.assertEqual(expect, self.test.us08())
-
-    def test_us01(self):
-        expect = {'@I32@', '@F4@', '@I6@', '@I31@', '@I7@', '@I14@', '@I34@'}
-        self.assertEqual(self.test.us01(), expect)
-
-    def test_us05(self):
-        expect = {'@F4@'}
-        self.assertEqual(self.test.us05(), expect)
-
-    def test_us14(self):
-        expect = ['@F9@']
-        self.assertEqual(expect, self.test.us09())
-
-    def test_us15(self):
-        expect = ['@F9@']
-        self.assertEqual(expect, self.test.us09())
+    def test_us11(self):
+        expect = {'@I1@'}
+        self.assertEqual(expect, self.test.us11())
 
     def test_us12(self):
         result = self.test.us12()
         self.assertIn('@F4@', result)
         self.assertIn('@F2@', result)
+
+    def test_us13(self):
+        expect = {'@F2@', '@F5@'}
+        self.assertEqual(expect, self.test.us13())
+
+    def test_us14(self):
+        expect = ['@F2@']
+        self.assertEqual(expect, self.test.us14())
+
+    def test_us15(self):
+        expect = ['@F2@']
+        self.assertEqual(expect, self.test.us15())
 
     def test_us16(self):
         result = self.test.us16()
@@ -80,14 +88,6 @@ class UserStoryTest(unittest.TestCase):
     def test_us20(self):
         expect = ['@I33@', '@I34@', '@I36@', '@I11@']
         self.assertEqual(expect, self.test.us20())
-
-    def test_us11(self):
-        expect = {'@I1@'}
-        self.assertEqual(expect, self.test.us11())
-    
-    def test_us13(self):
-        expect = {'@F2@', '@F5@'}
-        self.assertEqual(expect, self.test.us13())
 
 
 if __name__ == '__main__':
